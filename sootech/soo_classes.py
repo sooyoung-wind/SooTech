4b11b04643533093ac1acb70e6322818430a463c
SooY <sooyoung.wind@gmail.com>
2024-11-22 18:02:13 +0900

    Update soo_classes.py

--- sootech/soo_classes.py
+++ sootech/soo_classes.py
@@ -1,7 +1,7 @@
 import sys
 import types
 import pandas as pd
-import .soo_functions, .soo_statistic, .soo_plots
+from sootech import soo_functions, soo_statistic, soo_plots
 
 class SooTech:
     """
