2b5bbc0182bec71d25ac8bd5ffd0bfdef428bcc7
SooY <sooyoung.wind@gmail.com>
2024-11-22 17:56:02 +0900

    Update __init__.py

--- sootech/__init__.py
+++ sootech/__init__.py
@@ -1,6 +1,7 @@
-from .soo_functions import resumetable, seed_everything
-from .soo_statistic import me, mae, rmse, d, trans_to_UV, trans_to_WSWD, wd_diff_cal, mean_wd, wd_statistic, cal_statistics
+from .soo_functions import resumetable, seed_everything, trans_to_UV, trans_to_WSWD
+from .soo_statistic import me, mae, rmse, d, trans_to_WSWD, wd_diff_cal, mean_wd, wd_statistic, cal_statistics
 from .soo_plots import plot_performance_metrics
+from .soo_classes import SooTech
 
 __all__ = [
     "resumetable",
@@ -15,5 +16,6 @@ __all__ = [
     "mean_wd",
     "wd_statistic",
     "cal_statistics",
-    "plot_performance_metrics"
+    "plot_performance_metrics",
+    "SooTech"
 ]
